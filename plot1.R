8d52aa4a5c01eb51fbd9fbaf9e82c72242f883aa
c-breaux <53301234+c-breaux@users.noreply.github.com>
2019-11-18 11:02:45 -0500

    plot1 function

---
+++ plot1.R
@@ -0,0 +1,12 @@
+plot1 <- function(){
+  #read data
+  power_consumption <- read.table("~/Data Analysis Project/household_power_consumption.txt", header=TRUE, sep=";")
+
+  #subset data
+  consumption_select<-subset(power_consumption, power_consumption$Date=="1/2/2007" | power_consumption$Date=="2/2/2007")
+  
+  #basic plot function to export as png
+  png("plot1.png", width=480, height=480)
+  hist(as.numeric(as.character(consumption_select$Global_active_power)), col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+  dev.off()
+}
