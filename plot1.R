5b968f5551c5acc92bafe086ead3e047875a6c57
c-breaux <53301234+c-breaux@users.noreply.github.com>
2019-11-18 11:03:41 -0500

    added comments

--- plot1.R
+++ plot1.R
@@ -1,6 +1,6 @@
 plot1 <- function(){
-  #read data
-  power_consumption <- read.table("~/Data Analysis Project/household_power_consumption.txt", header=TRUE, sep=";")
+  #read data (assumes data is downloaded and unzipped in the default directory)
+  power_consumption <- read.table("~/household_power_consumption.txt", header=TRUE, sep=";")
 
   #subset data
   consumption_select<-subset(power_consumption, power_consumption$Date=="1/2/2007" | power_consumption$Date=="2/2/2007")
